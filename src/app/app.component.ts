007e7000ee9cb2fc55221222d4517eae888955d6
timswinkels <timswinkels@gmail.com>
2017-08-06 20:52:23 +0200

    Add overall styling and style albums list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 import { SpotifyAuthenticationService } from './spotify/services/authentication/authentication.service';
 
@@ -7,7 +7,8 @@ import * as queryString from 'querystring';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class AppComponent implements OnInit {
 
