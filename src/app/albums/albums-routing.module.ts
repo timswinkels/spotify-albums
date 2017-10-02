6b365194c695960636cb81f24a5dd427717c4b75
Tim Swinkels <tim.swinkels@itility.nl>
2017-10-02 19:21:28 +0200

    Add basic routes

--- src/app/albums/albums-routing.module.ts
+++ src/app/albums/albums-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { AlbumsListComponent } from './components/list/list.component';
+
+const routes: Routes = [
+  {
+    path: 'albums',
+    component: AlbumsListComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class AlbumsRoutingModule { }
+export class AlbumsRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+
 
 import { AlbumsModule } from './albums/albums.module';
 import { SaModule } from './sa/sa.module';
@@ -12,6 +14,7 @@ import { SpotifyModule } from './spotify/spotify.module';
     AppComponent
   ],
   imports: [
+    AppRoutingModule,
     BrowserModule,
     AlbumsModule,
     SaModule,
